022873fc7c6ae6d5d385420dafca6b4641be0b34
Romuald Brillout <github.com@brillout.com>
2018-11-13 23:20:17 +0100

    add support for undefined

---
+++ index.js
@@ -0,0 +1,8 @@
+const assert = require('reassert');
+
+assert.usage(
+  false,
+  "Instead of `require('@brillout/jpp'), do:",
+  "   `require('brillout/jpp/stringify')`, or",
+  "   `require('brillout/jpp/parse')`",
+);
--- parse.js
+++ parse.js
@@ -3,6 +3,20 @@ const reviver = require('./reviver');
 module.exports = parse;
 
 function parse(...args) {
-  args[1] = args[1] || reviver;
-  return JSON.parse.apply(JSON, args);
+  const obj = JSON.parse.apply(JSON, args);
+  // We don't use the reviver option in `JSON.parse(obj, reviver)`
+  // because we want the reviver to be able to change a value to undefined
+  modifier(obj, reviver);
+  return obj;
+}
+
+function modifier(obj, reviver) {
+  if( ! (obj instanceof Object) ) {
+    return obj;
+  }
+  Object.entries(obj)
+  .forEach(([key, val]) => {
+    obj[key] = reviver(key, val);
+    modifier(obj[key]);
+  });
 }
