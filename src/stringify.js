f797b2a0a0184d5ededcbddb8fe63b43da0f6c66
Romuald Brillout <github.com@brillout.com>
2018-11-15 15:58:20 +0100

    clean Data.prototype in finally block

--- src/stringify.js
+++ src/stringify.js
@@ -1,5 +1,4 @@
 const replacer = require('./replacer');
-//const assert = require('reassert');
 
 module.exports = stringify;
 
@@ -7,8 +6,11 @@ function stringify(...args) {
   args[1] = args[1] || replacer;
   const toJSON_org = Date.prototype.toJSON;
   Date.prototype.toJSON = function(key, val){ return replacer(key, this); };
-  const result = JSON.stringify.apply(JSON, args);
-  Date.prototype.toJSON = toJSON_org;
+  let result;
+  try {
+    result = JSON.stringify.apply(JSON, args);
+  } finally {
+    Date.prototype.toJSON = toJSON_org;
+  }
   return result;
 }
-
