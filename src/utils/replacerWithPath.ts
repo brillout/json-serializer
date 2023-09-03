a794bb5619ff15ed279eedf03cd700b3904bf328
Romuald Brillout <git@brillout.com>
2023-09-03 19:27:22 +0200

    chore

--- src/utils/replacerWithPath.ts
+++ src/utils/replacerWithPath.ts
@@ -12,7 +12,7 @@ function replacerWithPath(
   const paths = new WeakMap<Iterable, string>()
   return function (this: Iterable, key: string, value: unknown) {
     const prefix = paths.get(this)
-    const path = (prefix ?? '') + (key ? getKeyName(key, this, !prefix && canBeFirstKey) : '')
+    const path = (prefix ?? '') + (key ? getPropAccessNotation(key, this, !prefix && canBeFirstKey) : '')
     if (isIterable(value)) paths.set(value, path)
     return replacer.call(this, key, value, path)
   }
@@ -20,8 +20,8 @@ function replacerWithPath(
 function isIterable(value: unknown): value is Iterable {
   return value === Object(value)
 }
-function getKeyName(key: string, obj: Iterable, isFirstKey: boolean) {
+function getPropAccessNotation(key: string, obj: Iterable, isFirstKey: boolean) {
   if (Array.isArray(obj)) return `[${key}]`
-  if (isKeyDotNotationCompatible(key)) return !isFirstKey ? `.${key}` : key
-  return `[${JSON.stringify(key)}]`
+  if (isKeyDotNotationCompatible(key)) return !isFirstKey ? `.${key}` : key // Dot notation
+  return `[${JSON.stringify(key)}]` // Bracket notation
 }
