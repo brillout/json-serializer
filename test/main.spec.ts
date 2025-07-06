527d623c0db1bed0b6c30af6d94c24a1cf77de28
Romuald Brillout <git@brillout.com>
2025-07-06 23:20:41 +0200

    minor refactor

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,6 +1,6 @@
 import { parse } from '../src/parse'
 import { stringify } from '../src/stringify'
-import { describe, it } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import assert from 'assert'
 
 describe('escaping', () => {
@@ -8,9 +8,26 @@ describe('escaping', () => {
     ;['!undefined', '!Date:2021-01-12T21:22:42.143Z', '!NaN', '!Infinity', `!RegExp:/^\d+$/g`].forEach((val) => {
       assert(stringify(val) === `"!${val}"`)
       assert(parse(stringify(val)) === val)
-      assert(parse(stringify({ val })).val === val)
-      assert(parse(stringify({ val: { val } })).val.val === val)
+      assert((parse(stringify({ val })) as any).val === val)
+      assert((parse(stringify({ val: { val } })) as any).val.val === val)
     })
+  })
+})
+
+describe('user-defined replacer', () => {
+  it('works', () => {
+    expect(
+      stringify(
+        { icon: 'import:/assets/logo.svg:default', title: 'Hello' },
+        {
+          replacer(_key, value) {
+            if (typeof value === 'string' && value.startsWith('import:')) {
+              return { replacement: 'import42' }
+            }
+          },
+        },
+      ),
+    ).toBe('{"icon":"import42","title":"Hello"}')
   })
 })
 
@@ -27,12 +44,12 @@ describe('sortObjectKeys option', () => {
         assert(keys[0] === 'b')
       }
       {
-        const copy = parse(stringify(obj))
+        const copy = parse(stringify(obj)) as object
         const keys = Object.keys(copy)
         assert(keys[0] === 'b')
       }
       {
-        const copy = parse(stringify(obj, { sortObjectKeys: true }))
+        const copy = parse(stringify(obj, { sortObjectKeys: true })) as object
         const keys = Object.keys(copy)
         assert(keys[0] === 'a')
       }
@@ -51,12 +68,12 @@ describe('sortObjectKeys option', () => {
         assert(keys[0] === 'd')
       }
       {
-        const copy = parse(stringify(obj))
+        const copy = parse(stringify(obj)) as typeof obj
         const keys = Object.keys(copy.a)
         assert(keys[0] === 'd')
       }
       {
-        const copy = parse(stringify(obj, { sortObjectKeys: true }))
+        const copy = parse(stringify(obj, { sortObjectKeys: true })) as typeof obj
         const keys = Object.keys(copy.a)
         assert(keys[0] === 'c')
       }
