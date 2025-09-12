93609c251bbd263a40c90581df74e4688aa0548a
Romuald Brillout <git@brillout.com>
2025-09-12 14:51:57 +0200

    reproduce

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -69,6 +69,7 @@ describe('user-defined replacer', () => {
       deepOutsideMap: {
         f: '/foo/',
       },
+      someDate: new Date(2025)
     }
 
     const objStr = stringify(obj, {
@@ -80,7 +81,7 @@ describe('user-defined replacer', () => {
     })
 
     expect(objStr).toMatchInlineSnapshot(
-      `"{"pageId":"\\\\/pages\\\\/index","a":"!!\\\\/ hello","b":"!! world","nested":"!Map:[[\\"pageId\\",\\"\\\\\\\\/pages\\\\\\\\/about\\"],[\\"c\\",\\"!!!\\\\\\\\/ escaped\\"],[\\"deeply\\",{\\"pageId\\":\\"\\\\\\\\/pages\\\\\\\\/bla\\",\\"d\\":\\"!!\\\\\\\\/ also escaped\\",\\"deeper\\":{\\"e\\":\\"\\\\\\\\/ bla\\\\\\\\/\\"}}]]","deepOutsideMap":{"f":"\\\\/foo\\\\/"}}"`,
+      `"{"pageId":"\\\\/pages\\\\/index","a":"!!\\\\/ hello","b":"!! world","nested":"!Map:[[\\"pageId\\",\\"\\\\\\\\/pages\\\\\\\\/about\\"],[\\"c\\",\\"!!!\\\\\\\\/ escaped\\"],[\\"deeply\\",{\\"pageId\\":\\"\\\\\\\\/pages\\\\\\\\/bla\\",\\"d\\":\\"!!\\\\\\\\/ also escaped\\",\\"deeper\\":{\\"e\\":\\"\\\\\\\\/ bla\\\\\\\\/\\"}}]]","deepOutsideMap":{"f":"\\\\/foo\\\\/"},"someDate":"1970-01-01T00:00:02.025Z"}"`,
     )
 
     expect(
